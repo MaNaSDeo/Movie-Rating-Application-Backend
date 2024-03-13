528d231edd20f37e95388c57160a2b2aa7425e03
MaNaSDeo <rmnslogin@gmail.com>
2024-03-13 23:42:50 +0530

    Minor Tweaks

--- models/User.js
+++ models/User.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true
-        }
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-userSchema.methods.comparePassword = async function(candidatePassword){
-    return await bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
